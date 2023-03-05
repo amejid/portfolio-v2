6fbdc3c60dc06134bef4f3c184586dcc6371290b
amejid <abdelmejid89@gmail.com>
2023-03-05 20:32:35 +0300

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Amejid&apos;s Portfolio</title>
+      </Head>
+      <h1 className="text-red-500">Nice portfolio</h1>
+    </>
+  );
+}
